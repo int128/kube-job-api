08e77f7f9dcaf29c71a076c2445d4f688a93ac2a
Hidetake Iwata <int128@gmail.com>
2022-11-23 22:03:01 +0900

    Extract manager package (#4)

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,61 +1,48 @@
 package server
 
 import (
-	"flag"
+	"context"
+	"errors"
 	"fmt"
+	"net"
+	"net/http"
 
-	"go.uber.org/zap/zapcore"
-	"k8s.io/apimachinery/pkg/runtime"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"github.com/int128/kube-job-server/pkg/handlers"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/healthz"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-var (
-	scheme = runtime.NewScheme()
-)
-
-func init() {
-	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+type Server struct {
+	K8sClient client.Client
+	Addr      string
 }
 
-func Run() error {
-	var metricsAddr string
-	var probeAddr string
-	var apiAddr string
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.StringVar(&apiAddr, "api-bind-address", ":3000", "The address the api endpoint binds to.")
-	opts := zap.Options{
-		Development: true,
-		TimeEncoder: zapcore.RFC3339NanoTimeEncoder,
-	}
-	opts.BindFlags(flag.CommandLine)
-	flag.Parse()
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+func (s Server) Start(ctx context.Context) error {
+	logger := ctrl.LoggerFrom(ctx).WithName("http-server")
+	ctrl.LoggerInto(ctx, logger)
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		HealthProbeBindAddress: probeAddr,
-	})
-	if err != nil {
-		return fmt.Errorf("unable to start manager: %w", err)
-	}
+	m := http.NewServeMux()
+	m.Handle("/jobs/start", handlers.StartJob{K8sClient: s.K8sClient})
+	m.Handle("/jobs/status", handlers.GetJobStatus{K8sClient: s.K8sClient})
 
-	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
-		return fmt.Errorf("unable to set up health check: %w", err)
+	sv := http.Server{
+		BaseContext: func(listener net.Listener) context.Context { return ctx },
+		Addr:        s.Addr,
+		Handler:     m,
 	}
-	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
-		return fmt.Errorf("unable to set up ready check: %w", err)
-	}
-	if err := mgr.Add(httpServer{k8sClient: mgr.GetClient(), addr: apiAddr}); err != nil {
-		return fmt.Errorf("unable to add http server: %w", err)
-	}
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-		return fmt.Errorf("problem running manager: %w", err)
+	go func() {
+		<-ctx.Done()
+		logger.Info("Stopping server")
+		if err := sv.Close(); err != nil {
+			logger.Error(err, "could not close server")
+		}
+	}()
+	logger.Info("Starting server", "addr", sv.Addr)
+	if err := sv.ListenAndServe(); err != nil {
+		if errors.Is(err, http.ErrServerClosed) {
+			return nil
+		}
+		return fmt.Errorf("could not start http server: %w", err)
 	}
 	return nil
 }
