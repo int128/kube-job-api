4535909ffd99aa34c3c97048ecdae2bd040e25c8
Hidetake Iwata <int128@gmail.com>
2022-11-28 09:08:05 +0900

    Scaffold kubebuilder (#11)
    * kubebuilder init --domain int128.github.io --repo github.com/int128/kube-job-server
    
    * kubebuilder create api --group kubejobserver --version v1 --kind HTTPServer
    
    * Update deps

---
+++ controllers/httpserver_controller.go
@@ -0,0 +1,72 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/int128/kube-job-server/pkg/handlers"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// HTTPServerController manages a HTTPServer
+type HTTPServerController struct {
+	client.Client
+	Scheme *runtime.Scheme
+	Addr   string
+}
+
+// Start starts the HTTPServer
+func (r *HTTPServerController) Start(ctx context.Context) error {
+	m := http.NewServeMux()
+	m.Handle("/jobs/start", handlers.StartJob{K8sClient: r.Client})
+	m.Handle("/jobs/status", handlers.GetJobStatus{K8sClient: r.Client})
+
+	logger := ctrl.LoggerFrom(ctx).WithName("job-server")
+	ctrl.LoggerInto(ctx, logger)
+	sv := http.Server{
+		BaseContext: func(net.Listener) context.Context { return ctx },
+		Addr:        r.Addr,
+		Handler:     m,
+	}
+	go func() {
+		<-ctx.Done()
+		logger.Info("Stopping server")
+		if err := sv.Close(); err != nil {
+			logger.Error(err, "could not close server")
+		}
+	}()
+	logger.Info("Starting server", "addr", sv.Addr)
+	if err := sv.ListenAndServe(); err != nil {
+		if errors.Is(err, http.ErrServerClosed) {
+			return nil
+		}
+		return fmt.Errorf("could not start http server: %w", err)
+	}
+	return nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *HTTPServerController) SetupWithManager(mgr ctrl.Manager) error {
+	return mgr.Add(r)
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/int128/kube-job-server
 go 1.19
 
 require (
-	go.uber.org/zap v1.23.0
+	github.com/onsi/ginkgo/v2 v2.1.6
+	github.com/onsi/gomega v1.20.1
 	k8s.io/api v0.25.4
 	k8s.io/apimachinery v0.25.4
 	k8s.io/client-go v0.25.4
@@ -12,6 +13,13 @@ require (
 )
 
 require (
+	cloud.google.com/go v0.97.0 // indirect
+	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
+	github.com/Azure/go-autorest/autorest v0.11.27 // indirect
+	github.com/Azure/go-autorest/autorest/adal v0.9.20 // indirect
+	github.com/Azure/go-autorest/autorest/date v0.3.0 // indirect
+	github.com/Azure/go-autorest/logger v0.2.1 // indirect
+	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
@@ -26,6 +34,7 @@ require (
 	github.com/go-openapi/jsonreference v0.19.5 // indirect
 	github.com/go-openapi/swag v0.19.14 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang-jwt/jwt/v4 v4.2.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/gnostic v0.5.7-v3refs // indirect
@@ -48,6 +57,8 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.21.0 // indirect
+	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd // indirect
 	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
 	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
